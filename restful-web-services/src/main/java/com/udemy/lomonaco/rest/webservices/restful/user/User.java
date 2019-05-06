06b9c7930204c470ed8b11e7944c5d5799995129
Fernando G Lomonaco <fernando_lomonaco@outlook.com>
2019-05-05 23:12:27 -0300

    add security and user entity

--- restful-web-services/src/main/java/com/udemy/lomonaco/rest/webservices/restful/user/User.java
+++ restful-web-services/src/main/java/com/udemy/lomonaco/rest/webservices/restful/user/User.java
@@ -2,6 +2,9 @@ package com.udemy.lomonaco.rest.webservices.restful.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -9,8 +12,11 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description = "Detalhes sobre o usuario")
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 	@Size(min = 2, message = "Nome deve ter ao menos 2 caracteres")
 	@ApiModelProperty(notes = "Nome deve ter ao menos 2 caracteres")
